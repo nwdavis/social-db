395261e9e5154418a9d2afc24cfe6667b44ec7da
nwdavis <nathanieldavis89@gmail.com>
2017-09-17 17:01:28 -0400

    adding sql2 to packages, changing server.js

---
+++ server.js
@@ -0,0 +1,32 @@
+
+//dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+
+//setting up express app
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+//setting up Express app data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+//static directory
+app.use(express.static("app/public"));
+
+//routes
+require("./routes/api-routes.js")(app);
+
+//setting handlebars
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+//starts the server
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
+
