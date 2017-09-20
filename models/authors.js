93b1d78ae6507072ed3201a15a710c8155caf702
adurha10 <alamadu82@gmail.com>
2017-09-19 21:20:34 -0400

    Structured posts, authors and comment data

--- models/authors.js
+++ models/authors.js
@@ -1,7 +1,22 @@
 module.exports = function(sequelize, DataTypes) {
   var Author = sequelize.define("Author", {
     // Giving the Author model a name of type STRING
-    name: DataTypes.STRING
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    upVote: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    },
+    downVote: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    },
+    totalVote: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    }
   });
   return Author;
 };
