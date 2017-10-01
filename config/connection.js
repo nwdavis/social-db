7a6e46a6f48ea8c2cb09468ac0d52170d78dd950
nwdavis <nathanieldavis89@gmail.com>
2017-10-01 18:19:02 -0400

    fixing conflicts

--- config/connection.js
+++ config/connection.js
@@ -3,11 +3,7 @@ var connection = mysql.createConnection({
     port: 3306,
     host: "localhost",
     user: "root",
-<<<<<<< HEAD
-    password: "test1",
-=======
     password: "",
->>>>>>> d481cfe404092b5256d4e64931389014abde9858
     database: "social_db"
 });
 connection.connect(function(err) {
